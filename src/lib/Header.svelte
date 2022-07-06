a553f1e951f09417e11a62fca860bf5e3c9b66a2
adelyumi <88289834+adelyumi@users.noreply.github.com>
2022-07-06 12:26:24 +0300

    Add files via upload

---
+++ src/lib/Header.svelte
@@ -0,0 +1,80 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<header>
+	<nav>
+		<ul>
+			<li class:active={$page.url.pathname === '/'}><a sveltekit:prefetch href="/">Home</a></li>
+			<li class:active={$page.url.pathname === '/about'}>
+				<a sveltekit:prefetch href="/about">About Me</a>
+			</li>
+			<li class:active={$page.url.pathname === '/projects'}>
+				<a sveltekit:prefetch href="/projects">My Projects</a>
+			</li>
+			<li class:active={$page.url.pathname === '/api'}>
+				<a sveltekit:prefetch href="/api">Random Dog</a>
+			</li>
+		</ul>
+	</nav>
+</header>
+
+<style>
+	header {
+		text-align: left;
+		background-color: #a4c2a5;
+	}
+
+
+	nav {
+		display: flex;
+		justify-content: center;
+	}
+
+	ul {
+		position: relative;
+		padding: 0;
+		margin: 0;
+		height: 3em;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		list-style: none;
+		background-size: contain;
+	}
+
+	li {
+		position: relative;
+		height: 100%;
+	}
+
+	li.active::before {
+		--size: 6px;
+		content: '';
+		width: 0;
+		height: 0;
+		position: absolute;
+		top: 0;
+		left: calc(50% - var(--size));
+		border: var(--size) solid transparent;
+		border-top: var(--size) solid #566246;
+	}
+
+	nav a {
+		display: flex;
+		height: 100%;
+		align-items: center;
+		padding: 0 1em;
+		color: #ffffff;
+		font-weight: 700;
+		font-size: 0.8rem;
+		text-transform: uppercase;
+		letter-spacing: 0.1em;
+		text-decoration: none;
+		transition: color 0.2s linear;
+	}
+
+	a:hover {
+		color: #566246;
+	}
+</style>
